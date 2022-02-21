005f2edeaab80220d35584323fd46a87e317ce85
landrzejewski <landrzejewski.poczta@gmail.com>
2022-02-21 16:35:50 +0100

    update

--- shop-service/src/main/java/pl/training/shop/payments/logging/ConsolePaymentsLogger.java
+++ shop-service/src/main/java/pl/training/shop/payments/logging/ConsolePaymentsLogger.java
@@ -1,9 +1,9 @@
 package pl.training.shop.payments.logging;
 
 import lombok.extern.java.Log;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import pl.training.payments.Payment;
 import pl.training.payments.PaymentRequest;
 
 @Aspect
@@ -11,9 +11,23 @@ import pl.training.payments.PaymentRequest;
 @Log
 public class ConsolePaymentsLogger {
 
-    @Before("execution(* pl.training.shop.payments.TransactionalProcessPaymentUseCase.pro*(..)) && args(paymentRequest)")
+    @Pointcut("execution(* pl.training.shop.payments.TransactionalProcessPaymentUseCase.pro*(..))")
+    void processPayment() {
+    }
+
+    @Before("processPayment() && args(paymentRequest)")
     public void beforeProcessPayment(PaymentRequest paymentRequest) {
         log.info("New payment request: " + paymentRequest);
+    }
+
+    @AfterReturning(value = "processPayment()", returning = "payment")
+    public void onProcessPaymentSuccess(Payment payment) {
+        log.info("New payment : " + payment);
+    }
+
+    @AfterThrowing(value = "processPayment()", throwing = "exception")
+    public void onProcessPaymentSuccess(RuntimeException exception) {
+        log.info("Processing payment failed: " + exception);
     }
 
 }
