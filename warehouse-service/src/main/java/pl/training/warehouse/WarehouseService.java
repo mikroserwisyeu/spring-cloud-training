b99e4b95cb430d33aa881608ea77220bd3d82524
landrzejewski <landrzejewski.poczta@gmail.com>
2022-02-22 14:55:52 +0100

    update

---
+++ warehouse-service/src/main/java/pl/training/warehouse/WarehouseService.java
@@ -0,0 +1,13 @@
+package pl.training.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseService.class, args);
+    }
+
+}
