bad106b2a8809efe8e40812801e513acb9c77f4a
landrzejewski <landrzejewski.poczta@gmail.com>
2022-02-22 12:59:11 +0100

    update

--- shop-service/src/main/java/pl/training/shop/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/shop/ShopConfiguration.java
@@ -3,12 +3,7 @@ package pl.training.shop;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import pl.training.commons.FastMoneyMapper;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
-import static springfox.documentation.spi.DocumentationType.OAS_30;
 
 @Configuration
 public class ShopConfiguration {
@@ -18,12 +13,12 @@ public class ShopConfiguration {
         return Mappers.getMapper(FastMoneyMapper.class);
     }
 
-    @Bean
+   /* @Bean
     public Docket docket() {
         return new Docket(OAS_30)
                 .select()
                 .apis(withClassAnnotation(RestController.class))
                 .build();
-    }
+    }*/
 
 }
