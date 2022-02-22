f5ef5ab8188c6c9dbe73a30aef861f413553eb7d
landrzejewski <landrzejewski.poczta@gmail.com>
2022-02-22 09:14:28 +0100

    update

--- orders-api/src/main/java/pl/training/orders/ServiceUnavailableException.java
+++
@@ -1,9 +0,0 @@
-package pl.training.orders;
-
-public class ServiceUnavailableException extends RuntimeException{
-
-    public ServiceUnavailableException(Throwable cause) {
-        super(cause);
-    }
-
-}
