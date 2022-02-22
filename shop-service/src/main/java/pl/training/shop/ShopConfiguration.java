321d32b62ded3bcdaa83b432f4290dbada551a71
landrzejewski <landrzejewski.poczta@gmail.com>
2022-02-22 13:00:30 +0100

    update

--- shop-service/src/main/java/pl/training/shop/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/shop/ShopConfiguration.java
@@ -3,8 +3,10 @@ package pl.training.shop;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import pl.training.commons.FastMoneyMapper;
 
+@EnableTransactionManagement
 @Configuration
 public class ShopConfiguration {
 
